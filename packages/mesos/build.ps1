8361c2e0fbf7864ef1746e101fc78f025086f188
Paul Allen <paulall@microsoft.com>
2018-01-26 13:21:35 -0800

    clean up mesos build script

--- packages/mesos/build.ps1
+++ packages/mesos/build.ps1
@@ -12,35 +12,11 @@ param(
 
 )
 
-$branch = "paul-pkgpanda"
-
-# DCOS common configurations
-$LOG_SERVER_BASE_URL = "http://dcos-win.westus.cloudapp.azure.com"
-
 # Mesos configurationsG
-$MESOS_SERVICE_NAME = "dcos-mesos-slave"
-$MESOS_AGENT_PORT = 5051
 $MESOS_DIR = "c:\mesos-tmp"
-$MESOS_BIN_DIR = Join-Path $MESOS_DIR "bin"
-$MESOS_WORK_DIR = Join-Path $MESOS_DIR "work"
-$MESOS_LOG_DIR = Join-Path $MESOS_DIR "log"
-$MESOS_SERVICE_DIR = Join-Path $MESOS_DIR "service"
 $MESOS_BUILD_DIR = Join-Path $MESOS_DIR "build"
-$MESOS_BINARIES_DIR = Join-Path $MESOS_DIR "binaries"
 $MESOS_GIT_REPO_DIR = Join-Path $MESOS_DIR "mesos"
 $MESOS_BUILD_OUT_DIR = Join-Path $MESOS_DIR "build-output"
-$MESOS_BUILD_LOGS_DIR = Join-Path $MESOS_BUILD_OUT_DIR "logs"
-$MESOS_BUILD_BINARIES_DIR = Join-Path $MESOS_BUILD_OUT_DIR "binaries"
-$MESOS_BUILD_BASE_URL = "$LOG_SERVER_BASE_URL/mesos-build"
-
-# Tools installation directories
-$GIT_DIR = Join-Path $env:ProgramFiles "Git"
-$CMAKE_DIR = Join-Path $env:ProgramFiles "CMake"
-$VS2017_DIR = Join-Path ${env:ProgramFiles(x86)} "Microsoft Visual Studio\2017\Professional"
-$PATCH_DIR = Join-Path ${env:ProgramFiles(x86)} "GnuWin32"
-$PYTHON_DIR = Join-Path $env:SystemDrive "Python27"
-$PUTTY_DIR = Join-Path $env:ProgramFiles "PuTTY"
-$7ZIP_DIR = Join-Path $env:ProgramFiles "7-Zip"
 
 function Set-VCVariables {
     Param(
@@ -87,10 +63,7 @@ function New-Directory {
 function New-Environment {
     Write-Output "Creating new tests environment"
     New-Directory $MESOS_BUILD_DIR
-    New-Directory $MESOS_BINARIES_DIR
     New-Directory $MESOS_BUILD_OUT_DIR -RemoveExisting
-    New-Directory $MESOS_BUILD_LOGS_DIR
-#    Add-Content -Path $ParametersFile -Value "BRANCH=$Branch"
     Set-LatestMesosCommit    
     # Set Visual Studio variables based on tested branch
     Set-VCVariables "15.0"
@@ -137,46 +110,6 @@ function Start-ExternalCommand {
     }
 }
 
-function Set-LatestMesosCommit {
-    Push-Location $MESOS_GIT_REPO_DIR
-    try {
-        Start-ExternalCommand { git.exe log -n 1 } -ErrorMessage "Failed to get the latest commit message for the Mesos git repo" | Out-File "$MESOS_BUILD_LOGS_DIR\latest-commit.log"
-        $mesosCommitId = Start-ExternalCommand { git.exe log --format="%H" -n 1 } -ErrorMessage "Failed to get the latest commit id for the Mesos git repo"
-        Set-Variable -Name "LATEST_COMMIT_ID" -Value $mesosCommitId -Scope Global -Option ReadOnly
-    } finally {
-        Pop-Location
-    }
-}
-
-function Get-LatestCommitID {
-    if(!$global:LATEST_COMMIT_ID) {
-        Throw "Failed to get the latest Mesos commit ID. Perhaps it has not saved."
-    }
-    return $global:LATEST_COMMIT_ID
-}
-
-function Get-BuildOutputsUrl {
-    $mesosCommitID = Get-LatestCommitID
-    return "$MESOS_BUILD_BASE_URL/$Branch/$mesosCommitID"
-}
-
-function Get-BuildLogsUrl {
-    $buildOutUrl = Get-BuildOutputsUrl
-    return "$buildOutUrl/logs"
-}
-
-function Copy-CmakeBuildLogs {
-    Param(
-        [Parameter(Mandatory=$true)]
-        [string]$BuildName
-    )
-    Copy-Item "$MESOS_DIR\CMakeFiles\CMakeOutput.log" "$MESOS_BUILD_LOGS_DIR\$BuildName-CMakeOutput.log"
-    Copy-Item "$MESOS_DIR\CMakeFiles\CMakeError.log" "$MESOS_BUILD_LOGS_DIR\$BuildName-CMakeError.log"
-    if($global:BUILD_STATUS -eq 'FAIL') {
-        $logsUrl = Get-BuildLogsUrl
-        $global:LOGS_URLS += $("$logsUrl/$BuildName-CMakeOutput.log", "$logsUrl/$BuildName-CMakeError.log")
-    }
-}
 
 function Wait-ProcessToFinish {
     Param(
@@ -227,47 +160,29 @@ function Start-MesosCIProcess {
         [string]$ProcessPath,
         [Parameter(Mandatory=$false)]
         [string[]]$ArgumentList,
-#        [Parameter(Mandatory=$true)]
-#        [string]$StdoutFileName,
-#        [Parameter(Mandatory=$true)]
-#        [string]$StderrFileName,
         [Parameter(Mandatory=$true)]
         [string]$BuildErrorMessage
     )
-#    $stdoutFile = Join-Path $MESOS_BUILD_LOGS_DIR $StdoutFileName
-##    $stderrFile = Join-Path $MESOS_BUILD_LOGS_DIR $StderrFileName
- #   New-Item -ItemType File -Path $stdoutFile
- #   New-Item -ItemType File -Path $stderrFile
- #   $logsUrl = Get-BuildLogsUrl
- #   $stdoutUrl = "${logsUrl}/${StdoutFileName}"
- #   $stderrUrl = "${logsUrl}/${StderrFileName}"
     $command = $ProcessPath -replace '\\', '\\'
     if($ArgumentList.Count) {
         $ArgumentList | Foreach-Object { $command += " $($_ -replace '\\', '\\')" }
     }
     try {
         Wait-ProcessToFinish -ProcessPath $ProcessPath -ArgumentList $ArgumentList 
-#`
-#                             -StandardOutput $stdoutFile -StandardError $stderrFile
         $msg = "Successfully executed: $command"
     } catch {
         $msg = "Failed command: $command"
         $global:BUILD_STATUS = 'FAIL'
-        $global:LOGS_URLS += $($stdoutUrl, $stderrUrl)
         Write-Output "Exception: $($_.ToString())"
-#        Add-Content -Path $ParametersFile -Value "FAILED_COMMAND=$command"
         Throw $BuildErrorMessage
     } finally {
         Write-Output $msg
-  #      Write-Output "Stdout log available at: $stdoutUrl"
-  #      Write-Output "Stderr log available at: $stderrUrl"
     }
 }
 
 function Start-MesosBuild {
     Write-Output "Creating mesos cmake makefiles"
     Push-Location $MESOS_BUILD_DIR
-    $logsUrl = Get-BuildLogsUrl
     try {
         $generatorName = "Visual Studio 15 2017 Win64"
         $parameters = @("$MESOS_GIT_REPO_DIR", "-G", "`"$generatorName`"", "-T", "host=x64", "-DENABLE_LIBEVENT=1")
@@ -285,7 +200,6 @@ function Start-MesosBuild {
     try {
 
         Start-MesosCIProcess -ProcessPath "cmake.exe" -ArgumentList @("--build", ".", "--", "/m") -BuildErrorMessage "Mesos binaries failed to build."
-        #-StdoutFileName "mesos-binaries-build-cmake-stdout.log" -StderrFileName "mesos-binaries-build-cmake-stderr.log" `
     } finally {
         Pop-Location
     }
@@ -294,11 +208,6 @@ function Start-MesosBuild {
 
 }
 
-
-# Add all the tools to PATH
-$toolsDirs = @("$CMAKE_DIR\bin", "$GIT_DIR\cmd", "$GIT_DIR\bin", "$PYTHON_DIR",
-                "$PYTHON_DIR\Scripts", "$7ZIP_DIR", "$PATCH_DIR\bin")
-$env:PATH += ';' + ($toolsDirs -join ';')
 
 copy-item -Recurse "$pkgSrc" -destination "$MESOS_DIR"
 
