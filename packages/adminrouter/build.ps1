bf11c55070d99611975794fe11577bb3f04d900f
Paul Allen <paulall@microsoft.com>
2018-01-31 15:49:38 -0800

    fixed adminrouter packaging

--- packages/adminrouter/build.ps1
+++ packages/adminrouter/build.ps1
@@ -13,5 +13,5 @@ param(
 )
 
 copy-item "c:\pkg\src\Application-Request-Routing\requestRouter_amd64.msi" "$env:PKG_PATH"
-copy-item "c:\pkg\build\URLRewrite\rewrite_amd64-US.msi" "$env:PKG_PATH"
+copy-item "c:\pkg\src\URLRewrite\rewrite_amd64_en-US.msi" "$env:PKG_PATH"
 copy-item "c:\pkg\build\extra\*" "$env:PKG_PATH"
