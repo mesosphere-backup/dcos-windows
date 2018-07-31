45cad9b785ff930a6bad0a87af25955d2c6769ef
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2018-07-31 13:04:19 +0000

    Remove Spartan support
    DC/OS official Windows support will start with 1.11 release, which
    already has Spartan deprecated.

--- scripts/variables.ps1
+++ scripts/variables.ps1
@@ -1,12 +1,9 @@
 # DCOS common configurations
 $LOG_SERVER_BASE_URL = "http://dcos-win.westus.cloudapp.azure.com"
-$ERLANG_URL = "$LOG_SERVER_BASE_URL/downloads/erl8.3.zip"
 $ZOOKEEPER_PORT = 2181
 $EXHIBITOR_PORT = 8181
 $DCOS_DIR = Join-Path $env:SystemDrive "DCOS"
 $DCOS_ETC_DIR = Join-Path $env:SystemDrive "DCOS-etc"
-$ERLANG_DIR = Join-Path $DCOS_DIR "erl8.3"
-$ERTS_DIR = Join-Path $ERLANG_DIR "erts-8.3"
 $DOCKER_HOME = Join-Path $env:ProgramFiles "Docker"
 $DOCKER_DATA = Join-Path $env:ProgramData "Docker"
 $DOCKER_SERVICE_NAME = "Docker"
@@ -35,26 +32,6 @@ $MESOS_ETC_SERVICE_DIR = Join-Path $MESOS_ETC_DIR "service"
 # Amount of time to wait for an executor to register with the slave
 # before considering it hung and shutting it down (e.g., 60secs, 3mins, etc)
 $MESOS_REGISTER_TIMEOUT = "15mins"
-
-# EPMD configurations
-$EPMD_SERVICE_NAME = "dcos-epmd"
-$EPMD_SERVICE_DISPLAY_NAME = "DCOS EPMD Windows Agent"
-$EPMD_SERVICE_DESCRIPTION = "Windows Service for the DCOS EPMD Agent"
-$EPMD_PORT = 61420
-$EPMD_DIR = Join-Path $DCOS_DIR "epmd"
-$EPMD_LOG_DIR = Join-Path $EPMD_DIR "log"
-$EPMD_SERVICE_DIR = Join-Path $EPMD_DIR "service"
-
-# Spartan configurations
-$SPARTAN_LOCAL_ADDRESSES = @("198.51.100.1", "198.51.100.2", "198.51.100.3")
-$SPARTAN_SERVICE_NAME = "dcos-spartan"
-$SPARTAN_SERVICE_DISPLAY_NAME = "DCOS Spartan Windows Agent"
-$SPARTAN_SERVICE_DESCRIPTION = "Windows Service for the DCOS Spartan Windows Agent"
-$SPARTAN_DEVICE_NAME = "spartan"
-$SPARTAN_DIR = Join-Path $DCOS_DIR "spartan"
-$SPARTAN_LOG_DIR = Join-Path $SPARTAN_DIR "log"
-$SPARTAN_RELEASE_DIR = Join-Path $SPARTAN_DIR "release"
-$SPARTAN_SERVICE_DIR = Join-Path $SPARTAN_DIR "service"
 
 # Diagnostics configurations
 $DIAGNOSTICS_SERVICE_NAME = "dcos-diagnostics"
