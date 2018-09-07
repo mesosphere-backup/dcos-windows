7c1f42c99d716fb0dbb03a1bad52de8156a32b8b
Bruce Campbell <brcampbe@microsoft.com>
2018-09-07 12:10:43 -0700

    Added timer subclass. No action, just parse and stub

--- systemctl-win/src/service_unit.h
+++ systemctl-win/src/service_unit.h
@@ -22,6 +22,7 @@ using namespace std;
 #define MAX_BUFFER_SIZE 4096
 extern wchar_t BUFFER[];
 class SystemDUnit;
+class SystemDTimer;
 extern class SystemDUnitPool *g_pool;
 
 namespace SystemCtl {
@@ -57,6 +58,13 @@ namespace SystemCtlLog {
 
 class SystemDUnitPool {
 public:
+
+    enum UnitType {
+        UNIT_TYPE_INVALID = 0,
+        UNIT_TYPE_SERVICE,
+        UNIT_TYPE_TIMER
+    };
+
     SystemDUnitPool();
     ~SystemDUnitPool() { };
  
@@ -64,6 +72,7 @@ public:
     static class SystemDUnit *FindUnit(std::wstring name);
     static wstring FindServiceFilePath(wstring dir_path, wstring service_name); 
     static class SystemDUnit *ReadServiceUnit(std::wstring name, std::wstring service_unit_path);
+    static class SystemDTimer *ReadTimerUnit(std::wstring name, std::wstring service_unit_path);
 
     // Actual unit files only occur in the top directory. Various before, after, wants and requires directories are 
     // hard links to the actual unit file in ACTIVE_UNIT_DIRECTORY_PATH\\servicename
@@ -79,18 +88,19 @@ public:
     void SavePool() { };
     void ShowGlobal();
 
-   static wstring UNIT_DIRECTORY_PATH;        // Quasi constant inited in contructor
-   static wstring SERVICE_WRAPPER_PATH; // Single instance for everybody
-   static boolean DirExists(wstring dir_path);
-   static boolean Apply(wstring dir_path, boolean (*action)(wstring dir_path, void *context ), void *context);
+    static wstring UNIT_DIRECTORY_PATH;        // Quasi constant inited in contructor
+    static wstring SERVICE_WRAPPER_PATH; // Single instance for everybody
+    static boolean DirExists(wstring dir_path);
+    static boolean Apply(wstring dir_path, boolean (*action)(wstring dir_path, void *context ), void *context);
 
 private:
-   friend class SystemDUnit;
-   static wstring ACTIVE_UNIT_DIRECTORY_PATH; // Quasi constant
-   static wstring UNIT_WORKING_DIRECTORY_PATH; // Quasi constant
-   static std::map<std::wstring, class SystemDUnit *> pool;
+    friend class SystemDUnit;
+    friend class SystemDTimer;
+    static wstring ACTIVE_UNIT_DIRECTORY_PATH; // Quasi constant
+    static wstring UNIT_WORKING_DIRECTORY_PATH; // Quasi constant
+    static std::map<std::wstring, class SystemDUnit *> pool;
 
-   struct GlobalAttrs {
+    struct GlobalAttrs {
         std::wstring Version ;
         std::wstring Features;
         std::wstring Virtualization;
@@ -172,9 +182,6 @@ private:
    };
    
    struct GlobalAttrs globals;
-
-
-
 };
 
 
@@ -338,14 +345,16 @@ public:
         };
     ~SystemDUnit() {  };
 
-    boolean Enable(boolean block);
-    boolean Disable(boolean block);
-    boolean Mask(boolean block);
-    boolean Unmask(boolean block);
+    virtual SystemDUnitPool::UnitType UnitType() { return SystemDUnitPool::UNIT_TYPE_SERVICE; };
 
-    boolean IsEnabled() ;
-    boolean IsActive() ;
-    boolean IsFailed() ;
+    virtual boolean Enable(boolean block);
+    virtual boolean Disable(boolean block);
+    virtual boolean Mask(boolean block);
+    virtual boolean Unmask(boolean block);
+
+    virtual boolean IsEnabled() ;
+    virtual boolean IsActive() ;
+    virtual boolean IsFailed() ;
 
     wstring  &Name()  { return name; };
     wstring  &FilePath()  { return unit_file_path; };
@@ -436,26 +445,27 @@ public:
 
     static class SystemDUnit *ParseSystemDServiceUnit(wstring servicename, wstring unit_path, wifstream &fs);
  
-    boolean StartService(boolean blocking);
-    boolean StopService(boolean blocking);
-    boolean ReloadService(boolean blocking);
-    boolean RestartService(boolean blocking);
-    void ShowService();
+    virtual boolean StartService(boolean blocking);
+    virtual boolean StopService(boolean blocking);
+    virtual boolean ReloadService(boolean blocking);
+    virtual boolean RestartService(boolean blocking);
+    virtual void ShowService();
 
-    boolean RegisterService();
-    boolean UnregisterService();
-    boolean CheckForRequisites();
-    boolean WaitForAfters();
-    boolean Kill(int action, int killtarget, boolean block);
+    virtual boolean RegisterService();
+    virtual boolean UnregisterService();
+    virtual boolean CheckForRequisites();
+    virtual boolean WaitForAfters();
+    virtual boolean Kill(int action, int killtarget, boolean block);
 
    static void AddUserServiceLogonPrivilege();
 
-private:
-    wstring ParseUnitSection( wifstream &fs);
-    wstring ParseServiceSection( wifstream &fs);
-    wstring ParseInstallSection( wifstream &fs);
+protected:
+    virtual wstring ParseUnitSection( wifstream &fs);
+    virtual wstring ParseInstallSection( wifstream &fs);
+    virtual wstring ParseTimerSection( wifstream &fs);
+    virtual wstring ParseServiceSection( wifstream &fs);
 
-    void RegisterServiceProperties();
+    virtual void RegisterServiceProperties();
     DWORD GetMainPID();
 
     boolean ParseDuration(std::wstring str, double &millis);
@@ -463,7 +473,12 @@ private:
     wstring name;
     wstring description;
     wstring unit_file_path;
+
+
+private:
+
     boolean is_enabled;
+
     enum OUTPUT_TYPE output_type;
     enum OUTPUT_TYPE error_type;
     wstring   output_file_path;
@@ -576,41 +591,93 @@ private:
     static const unsigned long  ATTRIBUTE_BIT_USB_FUNCTION_DESCRIPTORS   =  0x08000000;
     static const unsigned long  ATTRIBUTE_BIT_USB_FUNCTION_STRINGS       =  0x10000000;
 
-    boolean SystemDUnit::attr_service_type( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_remain_after_exit( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_guess_main_pid( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_pid_file( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_bus_name( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_exec_start_pre( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_exec_start( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_exec_start_post( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_exec_stop( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_exec_stop_post( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_exec_reload( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_restart_sec( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_timeout_start_sec( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_timeout_stop_sec( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_timeout_sec( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_runtime_max_sec( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_watchdog_sec( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_restart( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_success_exit_status( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_restart_prevent_exit_status( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_restart_force_exit_status( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_permissions_start_only( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_root_directory_start_only( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_non_blocking( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_notify_access( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_sockets( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_file_descriptor_store_max( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_usb_function_descriptors( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_usb_function_strings( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_environment( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_environment_file( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_standard_output( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_standard_error( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
-    boolean SystemDUnit::attr_not_implemented( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_service_type( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_remain_after_exit( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_guess_main_pid( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_pid_file( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_bus_name( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_exec_start_pre( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_exec_start( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_exec_start_post( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_exec_stop( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_exec_stop_post( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_exec_reload( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_restart_sec( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_timeout_start_sec( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_timeout_stop_sec( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_timeout_sec( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_runtime_max_sec( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_watchdog_sec( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_restart( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_success_exit_status( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_restart_prevent_exit_status( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_restart_force_exit_status( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_permissions_start_only( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_root_directory_start_only( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_non_blocking( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_notify_access( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_sockets( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_file_descriptor_store_max( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_usb_function_descriptors( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_usb_function_strings( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_environment( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_environment_file( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_standard_output( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_standard_error( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_not_implemented( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
 
+};
+
+
+class SystemDTimer:public SystemDUnit {
+public: 
+
+    SystemDTimer(const wchar_t *name, const wchar_t *file_path = NULL): 
+         SystemDUnit(name, file_path) {
+    }
+
+    virtual SystemDUnitPool::UnitType UnitType() { return SystemDUnitPool::UNIT_TYPE_TIMER; }
+    static class SystemDTimer *ParseSystemDTimerUnit(wstring servicename, wstring unit_path, wifstream &fs);
+
+    wstring ParseTimerSection( wifstream &fs);
+
+    virtual boolean SystemDTimer::Enable(boolean block);
+    virtual boolean SystemDTimer::Disable(boolean block);
+    virtual boolean SystemDTimer::Kill(int action, int killtarget, boolean block);
+    virtual boolean SystemDTimer::Mask(boolean block);
+    virtual boolean SystemDTimer::Unmask(boolean block);
+    virtual void SystemDTimer::ShowService();
+
+protected:
+
+private:
+    typedef boolean (SystemDTimer::*systemd_timer_attr_func)( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    static std::map< std::wstring , SystemDTimer::systemd_timer_attr_func> SystemD_Timer_Attribute_Map;
+
+    double on_active_sec;
+    double on_boot_sec;
+    double on_startup_sec;
+    double on_unit_active_sec;
+    double on_unit_inactive_sec;
+    double accuracy_sec;
+    double randomized_delay_sec;
+    std::wstring unit; // Names of the units associated with this timer. 
+                       // Defaults to a single member named <servicename>.service
+    boolean persistent;
+    boolean wake_system;
+    boolean remain_after_elapse;
+
+    boolean attr_on_active_sec( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_on_boot_sec( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_on_startup_sec( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_on_unit_active_sec( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_on_unit_inactive_sec( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_accuracy_sec( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_randomize_delay_sec( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_unit( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_persistent( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_wake_system( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
+    boolean attr_remain_after_elapse( wstring attr_name, wstring attr_value, unsigned long &attr_bitmask );
 };
 
 
