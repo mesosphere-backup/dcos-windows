8d24679ef9f00a66239d277551efa2e5f8e966a7
Alin Gabriel Serdean <aserdean@ovn.org>
2018-10-04 13:47:18 +0300

    Make parsing timer units more tolerant
    If an invalid section is found in the unit just log an error, don't bail out
    from that point.
    
    Signed-off-by: Alin Gabriel Serdean <aserdean@ovn.org>

--- systemctl-win/src/timer_unit.cpp
+++ systemctl-win/src/timer_unit.cpp
@@ -177,9 +177,8 @@ class SystemDTimer *SystemDTimer::ParseSystemDTimerUnit(wstring servicename, wst
             if (line.length() == 0) {
                 break;
             }
-            SystemCtlLog::msg << L"invalid section heading " << line.c_str();
-            SystemCtlLog::Debug();
-            break;
+            SystemCtlLog::msg << L"Invalid section heading " << line.c_str();
+            SystemCtlLog::Warning();
         }
     }
    
